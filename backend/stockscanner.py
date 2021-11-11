19caa3ea72990c4b6fdf2fdc2cb7371017d8fa78
Abhay2412 <57297897+Abhay2412@users.noreply.github.com>
2021-11-11 13:06:46 -0700

    Finished all of the APIs assigned to me

--- backend/stockscanner.py
+++ backend/stockscanner.py
@@ -77,6 +77,80 @@ def showusers():
         userDetails = cur.fetchall()
         return jsonify({'username': userDetails})
 
+#-----------Offering API Calls----------------------------
+@app.route("/offering", methods = ['POST', 'GET'])
+def offering():
+    if request.method == 'POST':
+      cur = mysql.connection.cursor()
+      json = request.json
+
+      new_ID = json['ID']
+      new_Company_ID = json['Company_ID']
+      new_Prediction_ID = json['Prediction_ID']
+      new_Predict_Stock_Price = json['Predict_Stock_Price']
+      new_Strong_Buy = json['Strong_Buy']
+      new_Rating_Buy = json['Rating_Buy']
+      new_Rating_Sell = json['Rating_Sell']
+      new_Strong_Sell = json['Strong_Sell']
+      new_Rating_Hold = json['Rating_Hold']
+      new_Stock_Price = json['Stock_Price']
+      new_Sector = json['Sector']
+      new_Address = json['Address']
+      new_Founding_Date = json['Founding_Date']
+      new_Business_Name = json['Business_Name']
+
+      new_Offering_ID = json['Offering_ID']
+      new_Quantity_of_stock = json['Quantity_of_stock']
+      new_Price_offered_at = json['Price_offered_at']
+      new_Status_Complete = json['Status_Complete']
+      new_Status_Incomplete = json['Status_Incomplete']
+
+      cur.execute("INSERT INTO Prediction(P_ID) VALUES(%s)", ([new_Prediction_ID]))
+      cur.execute("INSERT INTO BUSINESS(Business_ID, Address, Founding_Date, Business_Name) VALUES(%s, %s, %s, %s)", (new_Company_ID, new_Address, new_Founding_Date, new_Business_Name))
+      cur.execute("INSERT INTO STOCK(ID, Company_ID, Prediction_ID, Predict_Stock_Price, Strong_Buy, Rating_Buy, Rating_Sell, Strong_Sell, Rating_Hold, Stock_Price, Sector) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (new_ID, new_Company_ID, new_Prediction_ID, new_Predict_Stock_Price, new_Strong_Buy, new_Rating_Buy, new_Rating_Sell, new_Strong_Sell, new_Rating_Hold, new_Stock_Price, new_Sector))
+
+      cur.execute("INSERT INTO OFFERING(Offering_ID, ID, Quantity_of_stock, Price_offered_at, Status_Complete, Status_Incomplete) VALUES(%s, %s, %s, %s, %s, %s)", (new_Offering_ID, new_ID, new_Quantity_of_stock, new_Price_offered_at, new_Status_Complete, new_Status_Incomplete))
+      
+      mysql.connection.commit()
+      cur.close()
+
+      return jsonify("Offering inserted successfully")
+    
+    if request.method == 'GET':
+        
+        cur = mysql.connection.cursor()
+
+        cur.execute("SELECT * FROM OFFERING")
+        
+        offering_row = cur.fetchall()
+        respone = jsonify({'Offering': offering_row})
+        respone.status_code = 200
+        
+        cur.close()
+        
+        return respone
+
+@app.route("/offering/<string:Offering_ID>", methods = ['DELETE'])
+def delete_offering(Offering_ID):
+
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM OFFERING WHERE Offering_ID = %s", ([Offering_ID]))
+
+    mysql.connection.commit()
+    cur.close()
+        
+    return jsonify("Offering deleted successfully")
+
+@app.route("/offering/<string:Offering_ID>", methods = ['GET'])
+def get_offering(Offering_ID):
+
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM OFFERING WHERE Offering_ID = %s", ([Offering_ID]))
+    specific_offering_details = cur.fetchall()
+    mysql.connection.commit()
+    cur.close()
+        
+    return jsonify({'Offering': specific_offering_details})
 
 #-----------Belongs To API Calls--------------------------
 @app.route("/belongsto", methods = ['POST', 'GET'])
