b3630e8a04f7f63b642d05c6e4620649c72566af
Abhay2412 <57297897+Abhay2412@users.noreply.github.com>
2021-12-13 14:29:36 -0700

    Created the forms for the Admin view, and finished the user contorl by the admin

--- backend/static/admin.js
+++ backend/static/admin.js
@@ -1,3 +1,15 @@
 document.getElementById("showAllUserButton").onclick = function () {
     location.href = "usersall";
 };
+
+document.getElementById("createUserButton").onclick = function () {
+    location.href = "addUserAdmin";
+};
+
+document.getElementById("deleteUserButton").onclick = function () {
+    location.href = "deleteUserAdmin";
+};
+
+document.getElementById("updateUserButton").onclick = function () {
+    location.href = "updateUserAdmin";
+};
