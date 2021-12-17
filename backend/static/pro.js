9e33443d08070c6b0c035e6da7d82eb9df0db887
Hassan <47461682+Hassan-A-K@users.noreply.github.com>
2021-12-16 17:00:43 -0700

    Create pro.js

---
+++ backend/static/pro.js
@@ -0,0 +1,15 @@
+document.getElementById("showAllOfferingButton").onclick = function () {
+    location.href = "offeringall";
+};
+
+document.getElementById("createOfferingButton").onclick = function () {
+    location.href = "addOfferingPro";
+};
+
+document.getElementById("deleteOfferingButton").onclick = function () {
+    location.href = "deleteOfferingPro";
+};
+
+document.getElementById("updateOfferingButton").onclick = function () {
+    location.href = "updateOfferingPro";
+};
